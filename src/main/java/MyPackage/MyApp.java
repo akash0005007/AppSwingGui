159cff7406c279822f1e534b42d6de05a4b98668
Akash <akash.chaudhary@esq.com>
2023-07-26 13:36:22 +0530

    commit

---
+++ src/main/java/MyPackage/MyApp.java
@@ -0,0 +1,91 @@
+package MyPackage;
+
+import javax.swing.*;
+
+public class MyApp implements Runnable {
+
+    private static MyApp aThread, bThread;
+    private char c;
+
+    public MyApp(char c) {
+        this.c = c;
+    }
+
+    public MyApp() {
+
+        aThread = new MyApp('a');
+        bThread = new MyApp('b');
+        Thread thread = new Thread(aThread);
+        Thread thread1 = new Thread(bThread);
+        thread.start();
+        thread1.start();
+        new MyGui();
+    }
+
+
+    @Override
+    public void run() {
+        if (this == aThread || this == bThread) {
+            System.out.println("hello");
+        }
+    }
+
+    public static void main(String[] args) {
+        new MyApp();
+    }
+}
+
+class MyGui {
+    private static JTextField num1TextField;
+    private static JTextField num2TextField;
+    private static JTextField resultTextField;
+
+    private static JButton addButton;
+    private static JPanel panel;
+
+    public MyGui() {
+        jFrame();
+        // Action listener for the "Add" button
+        addButton.addActionListener(e -> {
+            try {
+                int num1 = Integer.parseInt(num1TextField.getText());
+                int num2 = Integer.parseInt(num2TextField.getText());
+                int result = num1 + num2;
+                resultTextField.setText(Integer.toString(result));
+            } catch (NumberFormatException ex) {
+                resultTextField.setText("Invalid input");
+            }
+        });
+
+
+    }
+
+    public static void jFrame() {
+        jPanel();
+        JFrame frame = new JFrame("MyApp");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void jPanel() {
+        myComponents();
+        panel = new JPanel();
+        panel.add(new JLabel("Number 1:"));
+        panel.add(num1TextField);
+        panel.add(new JLabel("Number 2:"));
+        panel.add(num2TextField);
+        panel.add(addButton);
+        panel.add(new JLabel("Result:"));
+        panel.add(resultTextField);
+    }
+
+    public static void myComponents() {
+        num1TextField = new JTextField(10);
+        num2TextField = new JTextField(10);
+        resultTextField = new JTextField(10);
+        addButton = new JButton("Add");
+    }
+
+}
